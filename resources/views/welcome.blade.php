0d6bf7f8c068d1f944544babcda3b7e9bc0f8a1c
AsyiqinAkandi <83482774+AsyiqinAkandi@users.noreply.github.com>
2022-11-12 20:40:36 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,7 +37,7 @@
             @endif
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+                <div class="flex justify-center pt-8 sm:justify-start sm:pt-0:text-white">
                     <h1>Group 9</h1>
                 </div>
 
